87e5e307bd6289eab223e9d54c9982bbf65c1c49
timeisariver <bdthesun@gmail.com>
2026-08-10 08:08:51 +0900

    004_string

--- 0200-js-base/001_syntax/004_string.js
+++ 0200-js-base/001_syntax/004_string.js
@@ -3,28 +3,40 @@
  *
  */
 
-function length(str) {}
+function length(str) {
+  return str.length;
+}
 
 /**
  *  4.2 文字列のながさが偶数の場合にtrueを返却するメソッドを実装してください
  *
  */
 
-function lengthIsEven(str) {}
+function lengthIsEven(str) {
+  if (str.length % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 /**
  *  4.3 文字列の先頭一文字目を返却するメソッドを実装してください
  *
  */
 
-function firstChar(str) {}
+function firstChar(str) {
+  return str.slice(0, 1);
+}
 
 /**
  *  4.4 文字列の末尾一文字目を返却するメソッドを実装してください
  *
  */
 
-function lastChar(str) {}
+function lastChar(str) {
+  return str.slice(-1);
+}
 
 /**
  *  4.5 文字列と二つの数字a, bを渡すとa文字目から, b文字目まで
@@ -32,7 +44,9 @@ function lastChar(str) {}
  *
  */
 
-function substring(str, a, b) {}
+function substring(str, a, b) {
+  return str.slice(a - 1, b);
+}
 
 /**
  *  4.6 引数に与えられた二つの引数のうち、一つ目の引数の文字列に二つ目の引数の文字列が
@@ -45,14 +59,21 @@ function substring(str, a, b) {}
  *      "hogegeho", "fugafuga" => false
  * */
 
-function isInclude(a, b) {}
+function isInclude(a, b) {
+  return a.includes(b);
+}
 
 /**
  *  4.7 引数で渡された文字列を一文字ずつ表示するメソッドを実装してください
  *
  */
 
-function printByChar(str) {}
+function printByChar(str) {
+  const arr = str.split('');
+  arr.forEach((item) => {
+    console.log(item);
+  });
+}
 
 module.exports = {
   length,
