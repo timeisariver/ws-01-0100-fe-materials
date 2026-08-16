ef1ab26df8c66a8b0246c93d481b8253036bad32
timeisariver <bdthesun@gmail.com>
2026-08-16 13:26:32 +0900

    002_medium

--- 0200-js-base/003_practice/002_medium.js
+++ 0200-js-base/003_practice/002_medium.js
@@ -9,7 +9,18 @@
  *    'library', -1 => 'ibraryl'
  *
  */
-function rotate(str, num) {}
+function rotate(str, num) {
+  let rotatedWord = '';
+
+  // 後ろから何文字切り取るか
+  // マイナスの値がnumに入ってきた際、マイナスがマイナスを打ち消してプラスになる
+  rotatedWord += str.slice(-num);
+
+  // 後ろの num 文字を除いた残り（＝前半全部）
+  rotatedWord += str.slice(0, -num);
+
+  return rotatedWord;
+}
 
 /**
  *  母音を除いた文字列
@@ -22,7 +33,27 @@ function rotate(str, num) {}
  *    'banana' => 'bnn'
  *
  */
-function removeVowels(str) {}
+function removeVowels(str) {
+  let characters = str.split('');
+
+  characters = characters.filter((char) => {
+    return (
+      // char !== 'a' &&
+      // char !== 'i' &&
+      // char !== 'u' &&
+      // char !== 'e' &&
+      // char !== 'o'
+
+      // ↑自分で書いた奴
+      // ↓より良い書き方
+      !'aiueo'.includes(char)
+    );
+  });
+
+  const vowelsRemoved = characters.join('');
+
+  return vowelsRemoved;
+}
 
 /**
  *  文字列のカウント
@@ -35,7 +66,10 @@ function removeVowels(str) {}
  *    'hogehoage',  'hoge' => 1
  *
  */
-function countStr(s1, s2) {}
+function countStr(s1, s2) {
+  // split は断片の配列を返す。断片の数は区切りの数より必ず1多いので、1を引く
+  return s1.split(s2).length - 1;
+}
 
 /**
  *  引数に与えられたアルファベットの文字列が回文であること
@@ -49,7 +83,13 @@ function countStr(s1, s2) {}
  *
  */
 
-function isPalindrome(str) {}
+function isPalindrome(str) {
+  const characters = str.split('');
+  characters.reverse();
+  const reversedWord = characters.join('');
+
+  return str === reversedWord;
+}
 
 /**
  *  素数
@@ -65,7 +105,17 @@ function isPalindrome(str) {}
  *    11 => True
  *
  */
-function isPrime(num) {}
+function isPrime(num) {
+  // 素数とは1より大きい整数のうち、1と自分自身でしか割り切れない数
+
+  if (num < 2) return false;
+
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+
+  return true;
+}
 
 /**
  *  配列の4と次の数字を抜いた合計
@@ -82,7 +132,26 @@ function isPrime(num) {}
  *    [4] => 0
  *
  */
-function sumWithout4andNext(array) {}
+function sumWithout4andNext(array) {
+  let skipsNext = false;
+  let total = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 4) {
+      skipsNext = true;
+      continue;
+    }
+
+    if (skipsNext) {
+      skipsNext = false;
+      continue;
+    }
+
+    total += array[i];
+  }
+
+  return total;
+}
 
 module.exports = {
   rotate,
