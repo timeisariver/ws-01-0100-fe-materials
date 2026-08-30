1dc44ecd65f2f13e5823566397a6e8d1e3b1a865
timeisariver <bdthesun@gmail.com>
2026-08-31 03:30:09 +0900

    初稿

--- 0300-js-dom-todo/js/function.js
+++ 0300-js-dom-todo/js/function.js
@@ -4,66 +4,100 @@ const state = {
     {
       name: 'Task 1',
       deadline: new AppDate().getDateInXMonth(1),
+      completed: false,
     },
     {
       name: 'Task 2',
       deadline: new AppDate().getDateInXMonth(2),
+      completed: false,
     },
     {
       name: 'Task 3',
       deadline: new AppDate().getDateInXMonth(3),
+      completed: false,
     },
   ],
 };
 
 // ↓↓↓ ここを実装
 function renderTasks(container) {
+  removeHTML(container);
+
   state.tasks.forEach((task) => {
-    const tr = document.createElement('tr');
-    tr.classList.add('content__table-row');
+    if (!state.showCompleted && task.completed) return;
 
-    const td1 = document.createElement('td');
-    const checkbox1 = document.createElement('input');
-    checkbox1.type = 'checkbox';
-    td1.append(checkbox1);
+    // ロウ作成
+    const row = div('content__row');
 
-    const td2 = document.createElement('td');
-    td2.innerHTML = task.name;
+    // カラム1
+    const col1 = div('content__col');
+    const check = checkbox(task.completed, (checked) => {
+      task.completed = checked;
+      renderTasks(container);
+    });
+    col1.append(check);
+    row.append(col1);
 
-    const td3 = document.createElement('td');
-    const checkbox2 = document.createElement('input');
-    checkbox2.type = 'date';
-    checkbox2.value = task.deadline;
-    td3.append(checkbox2);
+    // カラム2
+    const col2 = div('content__col content__col--name');
+    const taskNameInput = document.createElement('input');
+    taskNameInput.type = 'text';
+    taskNameInput.value = task.name;
+    taskNameInput.addEventListener('change', (e) => {
+      task.name = e.target.value;
+    });
+    col2.append(taskNameInput);
+    row.append(col2);
 
-    const td4 = document.createElement('td');
-    const button = document.createElement('button');
-    const icon = document.createElement('i');
-    td4.append(button);
-    icon.classList.add('icon', 'icon--trash', 'fa-solid', 'fa-trash');
-    button.append(icon);
+    // カラム3
+    const col3 = div('content__col content__col--deadline');
+    const dateInput = document.createElement('input');
+    dateInput.type = 'date';
+    dateInput.value = task.deadline;
+    dateInput.addEventListener('change', (e) => {
+      task.deadline = AppDate.parse(e.target.value);
+    });
+    col3.append(dateInput);
+    row.append(col3);
 
-    tr.append(td1, td2, td3, td4);
-    container.append(tr);
+    // カラム4
+    const col4 = div('content__col');
+    const deleteIcon = icon('icon icon--trash fa-solid fa-trash', () => {
+      if (confirm('タスクを削除してもいいですか？')) {
+        const newTasks = state.tasks.filter((t) => t !== task);
+        state.tasks = newTasks;
+        renderTasks(container);
+      }
+    });
+    col4.append(deleteIcon);
+    row.append(col4);
+
+    // ロウ追加
+    container.append(row);
   });
 }
 
-function removeTasks(container) {
+function removeHTML(container) {
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
 }
 
 function onSubmitTask(container) {
-  const taskName = document.getElementById('js-task-name').value;
-  const taskDeadline = document.getElementById('js-task-deadline').value;
+  const inputName = document.getElementById('js-task-name');
+  const inputDeadline = document.getElementById('js-task-deadline');
 
   state.tasks.push({
-    name: taskName,
-    deadline: taskDeadline ? new AppDate(taskDeadline) : new AppDate(),
+    name: inputName.value,
+    deadline: inputDeadline.value
+      ? AppDate.parse(inputDeadline.value)
+      : new AppDate(),
+    completed: false,
   });
 
-  removeTasks(container);
+  inputName.value = '';
+  inputDeadline.value = '';
+
   renderTasks(container);
 }
 // ↑↑↑
